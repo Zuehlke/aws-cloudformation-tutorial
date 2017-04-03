430807dac740b7bd3d6d1771cc260d8c1d1ff89b
raphaellueckl <raphael.lueckl@gmx.ch>
2017-04-03 17:10:58 +0200

    Fighting with the linux bash

--- deployment_scripts/CleanupOldVersion.sh
+++ deployment_scripts/CleanupOldVersion.sh
@@ -1,5 +1,2 @@
 #!/bin/bash -ex
-[ -e appspec.yml ] && rm appspec.yml || echo "appspec.yml does not exist."
-[ -e deployment_scripts/AppStart.sh ] && rm deployment_scripts/AppStart.sh || echo "AppStart.sh does not exist."
-[ -e deployment_scripts/AppStop.sh ] && rm deployment_scripts/AppStop.sh || echo "AppStop.sh does not exist."
-[ -e src/Hi/build/libs/hi-1.0-SNAPSHOT.jar ] && rm src/Hi/build/libs/hi-1.0-SNAPSHOT.jar || echo "hi-1.0-SNAPSHOT.jar does not exist."
+rm -rf /opt/codedeploy-agent/deployment-root/
