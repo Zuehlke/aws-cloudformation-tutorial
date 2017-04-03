dcff2ae1552f84336c234ae0e832b7b88ee4a3c0
raphaellueckl <raphael.lueckl@gmx.ch>
2017-04-03 16:56:19 +0200

    Changes script to work

--- deployment_scripts/CleanupOldVersion.sh
+++ deployment_scripts/CleanupOldVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-[ -e appspec.yml ] && rm appspec.yml
-[ -e deployment_scripts/AppStart.sh ] && rm deployment_scripts/AppStart.sh
-[ -e deployment_scripts/AppStop.sh ] && rm deployment_scripts/AppStop.sh
-[ -e src/Hi/build/libs/hi-1.0-SNAPSHOT.jar ] && rm src/Hi/build/libs/hi-1.0-SNAPSHOT.jar
+[ -e appspec.yml ] && rm appspec.yml || echo "appspec.yml does not exist."
+[ -e deployment_scripts/AppStart.sh ] && rm deployment_scripts/AppStart.sh || echo "AppStart.sh does not exist."
+[ -e deployment_scripts/AppStop.sh ] && rm deployment_scripts/AppStop.sh || echo "AppStop.sh does not exist."
+[ -e src/Hi/build/libs/hi-1.0-SNAPSHOT.jar ] && rm src/Hi/build/libs/hi-1.0-SNAPSHOT.jar || echo "hi-1.0-SNAPSHOT.jar does not exist."
